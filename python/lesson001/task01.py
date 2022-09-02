920c6cdedda03d2ce3e681b8d5eb3d1a8ff2ce36
Eugene Menski <eugene.menski@gmail.com>
2022-09-02 18:04:17 +0300

    Check whether holiday or not

--- python/lesson001/task01.py
+++ python/lesson001/task01.py
@@ -5,3 +5,17 @@
 # 7 -> да
 # 1 -> нет
 
+user_input = input('Введите цифру: ')
+
+if user_input.isnumeric():
+    number = int(user_input)
+else:
+    number = -1
+    print('Некорректное значение!')
+
+if number == 0:
+    print('Цифра должна быть больше нуля!')
+if number > 0 and number <= 5:
+    print('Нет, это не выходной день!')
+elif number == 6 or number == 7:
+    print('Да, это выходной день!')
