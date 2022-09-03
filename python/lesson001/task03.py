c29eeeabb1fda3daf5fc363b694c4a5eb94a273a
Eugene Menski <eugene.menski@gmail.com>
2022-09-03 08:58:09 +0300

    Define quadrant by coordinates

--- python/lesson001/task03.py
+++ python/lesson001/task03.py
@@ -7,3 +7,35 @@
 # x=2; y=4-> 1
 # x=-34; y=-30 -> 3
 
+def getNumber(coordinate):
+    try:
+        number = float(input(f'Введите координату {coordinate}: '))
+    except:
+        print('Введенное значение не является числом!')
+    return number
+
+def isZero(number):
+    if number == 0:
+        return True
+    else:
+        return False
+
+def getQuadrant(x, y):
+    quadrant = None
+    if x > 0 and y > 0:
+        quadrant = 1
+    elif x < 0 and y > 0:
+        quadrant = 2
+    elif x < 0 and y < 0:
+        quadrant = 3
+    elif x > 0 and y < 0:
+        quadrant = 4
+    return quadrant
+
+x = getNumber('X')
+y = getNumber('Y')
+
+if isZero(x) or isZero(y):
+    print('Обе координаты должны быть отличны от нуля!')
+else:
+    print(f'Точка с координатами ({x}, {y}) находится в {getQuadrant(x, y)}-й четверти.')
