612725d16c7d86d894848eca6b417e474377a348
Eugene Menski <eugene.menski@gmail.com>
2022-09-03 09:30:26 +0300

    Get range of valid coordinates

--- python/lesson001/task04.py
+++ python/lesson001/task04.py
@@ -1,3 +1,31 @@
 # Напишите программу, которая по заданному номеру четверти,
 # показывает диапазон возможных координат точек в этой четверти (x и y).
 
+def get_quadrant():
+    try:
+        return int(input(f'Введите номер четверти: '))
+    except:
+        print('Некорректное значение!')
+        return -1
+
+def get_coord_range(quadrant):
+    if quadrant == 1:
+        x = (0, '+∞')
+        y = (0, '+∞')
+    elif quadrant == 2:
+        x = (0, '-∞')
+        y = (0, '+∞')
+    elif quadrant == 3:
+        x = (0, '-∞')
+        y = (0, '-∞')
+    elif quadrant == 4:
+        x = (0, '+∞')
+        y = (0, '-∞')
+    return (x, y)
+
+quadrant = get_quadrant()
+if quadrant in range(1, 5):
+    coordinates = get_coord_range(quadrant)
+    print(f"Диапазон возможных значений:\nПо оси OX: {coordinates[0]}\nПо оси OY: {coordinates[1]}")
+else:
+    print("Попробуйте снова!")
