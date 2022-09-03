338888e89e6a7bb97f3dc90e2961d82bff79c412
Eugene Menski <eugene.menski@gmail.com>
2022-09-03 09:12:50 +0300

    Calculate distance between 2 points

--- python/lesson001/task05.py
+++ python/lesson001/task05.py
@@ -4,3 +4,20 @@
 # A (3,6); B (2,1) -> 5,09
 # A (7,-5); B (1,-1) -> 7,21
 
+def getCoordinates(point):
+    print(f'Точка {point}')
+    try:
+        x = float(input(f'Введите координату X: '))
+        y = float(input(f'Введите координату Y: '))
+    except:
+        print('Введенное значение не является числом!')
+    return (x, y)
+
+def calcDistance(point_a, point_b):
+    distance = ((point_a[0] - point_b[0])**2 + (point_a[1] - point_b[1])**2)**0.5
+    return distance
+
+point_a = getCoordinates('A')
+point_b = getCoordinates('B')
+
+print(f"Расстояние между точками {point_a} и {point_b} равно {calcDistance(point_a, point_b):.3f}")
