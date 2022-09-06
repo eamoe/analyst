04efd01e86983407ec88e8a0eb0cfb9f2d851c88
Eugene Menski <eugene.menski@gmail.com>
2022-09-06 21:30:41 +0300

    Calc sum of digits

--- python/lesson002/task01.py
+++ python/lesson002/task01.py
@@ -1,5 +1,18 @@
 # Задание 1 Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 # Пример:
 # 6782 -> 23
-# 0,56 -> 11
+# 0.56 -> 11
 
+def calc_digits_sum(number_str):
+    digits_list = [*number_str.replace('-', '').replace('.', '')]
+    sum = 0
+    for digit in digits_list:
+        sum += int(digit)
+    return sum
+
+
+user_number_str = input("Введите вещественное число: ")
+
+user_number = float(user_number_str)
+
+print(f"Сумма цифр числа {user_number} равна {calc_digits_sum(user_number_str)}")
