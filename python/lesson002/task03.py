8f1f5b58e3eb0f86b0d055a13b25e4177a77fa35
Eugene Menski <eugene.menski@gmail.com>
2022-09-06 21:50:43 +0300

    Calc sum of series' elements

--- python/lesson002/task03.py
+++ python/lesson002/task03.py
@@ -3,3 +3,19 @@
 # Пример:
 # Для n = 6 -> 14.072
 
+
+number = int(input("Введите целое положительное число больше нуля: "))
+
+def form_items_list(number):
+    items_list = []
+    for n in range(1, number + 1):
+        items_list.append((1 + 1 / n) ** n)
+    return items_list
+
+def calc_items_sum(items):
+    sum = 0
+    for item in items:
+        sum += item
+    return sum
+
+print(f"Сумма {number} элементов последовательности (1 + 1/n)^n равна: {round(calc_items_sum(form_items_list(number)), 3)}")
