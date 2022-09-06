ca2a622c7e50edc155a064ad4b020d27fe2cf627
Eugene Menski <eugene.menski@gmail.com>
2022-09-06 21:40:08 +0300

    Calc factorials of a range

--- python/lesson002/task02.py
+++ python/lesson002/task02.py
@@ -2,3 +2,16 @@
 # Пример:
 # пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = int(input("Введите целое положительное число: "))
+
+factorial_list = []
+for i in range(1, number + 1):
+    factorial_list.append(factorial(i))
+
+print(f"Набор произведений для числа {number}: {factorial_list}")
