afbc327097bdb0960e8dbfbbc692917d088174fb
Eugene Menski <eugene.menski@gmail.com>
2022-09-06 22:55:53 +0300

    Create shuffle function

--- python/lesson002/task05.py
+++ python/lesson002/task05.py
@@ -1,2 +1,16 @@
 # Задание 5 Реализуйте алгоритм перемешивания списка.
 
+import random
+
+numbers_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def shuffle(numbers):
+    for n in range(len(numbers) - 1, 0, -1):
+        index = random.randint(0, n + 1)
+        temp = numbers[n]
+        numbers[n] = numbers[index]
+        numbers[index] = temp
+    return numbers
+	
+print ("Исходный список : " + str(numbers_list))
+print ("Перемешанный список : " + str(shuffle(numbers_list)))
