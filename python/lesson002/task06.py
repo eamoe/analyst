face876c60077d89ba63a6931645439f3975efce
Eugene Menski <eugene.menski@gmail.com>
2022-09-06 23:12:02 +0300

    Count the  number of occurences

--- python/lesson002/task06.py
+++ python/lesson002/task06.py
@@ -2,3 +2,14 @@
 # а программа - определять количество вхождений одной строки в другой.
 # Пример: "abababb" и "aba" -> 2
 
+def count_substrings(main_string, substring):
+      counter = 0
+      for pos in range(len(main_string) - len(substring) + 1):
+           if main_string[pos:pos + len(substring)] == substring:
+                counter += 1
+      return counter
+
+initial_str = input("Введите строку: ")
+sub_str = input("Введите подстроку: ")
+
+print(count_substrings(initial_str, sub_str))
