e04f1dadf52248295eb34a1b5a1e2176b3c2fdf2
Eugene Menski <eugene.menski@gmail.com>
2022-09-09 15:44:37 +0300

    Calc diff between max and min fractional part

--- python/lesson003/task03.py
+++ python/lesson003/task03.py
@@ -3,3 +3,27 @@
 # Пример:
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
+def get_fraction_length(number):
+    scale = 0
+    while number < 1:
+        number *= 10
+        scale += 1
+    return scale
+
+def max_diff_fraction(input_list):
+    min_fraction = input_list[0] - int(input_list[0])
+    max_fraction = input_list[0] - int(input_list[0])
+    for item in input_list:
+        if str(item).find('.') != -1:
+            fraction = item - int(item)
+            if fraction > max_fraction:
+                max_fraction = fraction
+            elif fraction < min_fraction:
+                min_fraction = fraction
+    output = int((max_fraction - min_fraction) * (10 ** get_fraction_length(min_fraction)))
+    return output / (10 ** get_fraction_length(min_fraction))
+
+input_list = [1.1, 1.2, 3.1, 5, 10.01]
+
+print(f"Список: {input_list}")
+print(f"Разница между максимальным и минимальным значением дробной части элементов равна: {max_diff_fraction(input_list)}")
