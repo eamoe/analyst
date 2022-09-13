fc667f4f6ada1763e05135801712b98d4b007f9b
Eugene Menski <eugene.menski@gmail.com>
2022-09-13 16:28:52 +0300

    number factorization

--- python/lesson004/task02.py
+++ python/lesson004/task02.py
@@ -1,3 +1,23 @@
 # Задайте натуральное число N.
 # Напишите программу, которая составит список простых множителей числа N.
 
+def find_factors(number):
+    div = 2
+    factors = []
+    while div <= number:
+        if number % div == 0:
+            factors.append(div)
+            number = number / div
+        else:
+            div = div + 1
+    return factors
+
+def print_expression(factors):
+    factors_str = " * "
+    return factors_str.join(map(str, factors))
+
+number = int(input("Введите натуральное число: "))
+
+factors = find_factors(number)
+print(f"Разложение числа {number} на простые множители:")
+print(f"{number} = {print_expression(factors)}")
