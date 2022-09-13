741e01e0853c96e4b7148e2ea439126d07773e63
Eugene Menski <eugene.menski@gmail.com>
2022-09-13 16:57:19 +0300

    For a list of unique numbers

--- python/lesson004/task03.py
+++ python/lesson004/task03.py
@@ -1,3 +1,26 @@
 # Задайте последовательность чисел.
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
+def form_unique_list(input_list):
+    
+    elements = input_list.copy()
+    
+    frequency_dict = {}
+    while len(elements) > 0:
+        element = elements.pop()
+        if element in frequency_dict:
+            frequency_dict[element] += 1
+        else:
+            frequency_dict[element] = 1
+    
+    unique_elements = []
+    for key, value in frequency_dict.items():
+        if value == 1:
+            unique_elements.append(key)
+    
+    return unique_elements
+
+input_list = [1, 2, 7, 3, 4, 7, 2, 0, 8, 1]
+
+print(f"Исходный список: {input_list}")
+print(f"Список неповторяющихся элементов: {form_unique_list(input_list)}")
